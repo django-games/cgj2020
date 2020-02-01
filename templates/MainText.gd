29938211e6b12ae6542437a3d24b637eb8c218a3
Andrés Ignacio Torres <andresitorresm@gmail.com>
2020-02-01 08:34:34 -0400

    fix var use

--- templates/MainText.gd
+++ templates/MainText.gd
@@ -1,11 +1,9 @@
 extends Label
 
-onready var SECONDS_TO_WAIT = get_node("/root/MainContainer").SECONDS_TO_WAIT
-onready var IS_READY = get_node("/root/MainContainer").IS_READY
-onready var IS_ACTIVE = get_node("/root/MainContainer").IS_ACTIVE
-onready var PLAY_SOUND = get_node("/root/MainContainer").PLAY_SOUND
-onready var TEXT = get_node("/root/MainContainer").TEXT
-onready var audio_stream = get_node("/root/MainContainer/Sound")
+onready var SECONDS_TO_WAIT = get_parent().get_parent().SECONDS_TO_WAIT
+onready var PLAY_SOUND = get_parent().get_parent().PLAY_SOUND
+onready var TEXT = get_parent().get_parent().TEXT
+onready var audio_stream = get_parent().get_node("../Sound")
 
 var TEXT_SIZE = -1
 var SECONDS_COUNTER = -1
@@ -18,13 +16,14 @@ func _ready():
 	self.visible_characters = 0
 
 func _process(delta):
-	if IS_ACTIVE:
+	if get_parent().get_parent().IS_ACTIVE:
 		SECONDS_COUNTER += delta
 		if SECONDS_COUNTER >= SECONDS_TO_WAIT:
 			if self.visible_characters <= TEXT_SIZE:
 				self.visible_characters += 1
-				if audio_stream != null and self.text[self.visible_characters - 1] != " " and PLAY_SOUND:
+				var current_pos = min(self.visible_characters, self.text.length())
+				if audio_stream != null and self.text[current_pos - 1] != " " and PLAY_SOUND:
 					audio_stream.play()
 			else:
-				IS_READY = true
+				get_parent().get_parent().IS_READY = true
 			SECONDS_COUNTER = 0
