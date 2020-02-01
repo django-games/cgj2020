3aca156b0d7985c6122bb99dbea8179359b6bd0a
Andrés Ignacio Torres <andresitorresm@gmail.com>
2020-02-01 02:20:43 -0400

    play typewriter sound on text

--- templates/MainText.gd
+++ templates/MainText.gd
@@ -2,8 +2,10 @@ extends Label
 
 export(float) var SECONDS_TO_WAIT = 0.25
 export(bool) var IS_READY = false
+export(bool) var PLAY_SOUND = true
 var TEXT_SIZE = -1
 var SECONDS_COUNTER = -1
+onready var audio_stream = get_node("/root/MainContainer/Sound")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,6 +19,8 @@ func _process(delta):
 	if SECONDS_COUNTER >= SECONDS_TO_WAIT:
 		if self.visible_characters <= TEXT_SIZE:
 			self.visible_characters += 1
+			if audio_stream != null and self.text[self.visible_characters - 1] != " " and PLAY_SOUND:
+				audio_stream.play()
 		else:
 			IS_READY = true
 		SECONDS_COUNTER = 0
