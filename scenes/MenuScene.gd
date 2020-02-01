d90808eeb87562175b9cddcf04dd2030f667251d
Andrés Ignacio Torres <andresitorresm@gmail.com>
2020-01-31 23:53:36 -0400

    add basic menu scene

---
+++ scenes/MenuScene.gd
@@ -0,0 +1,11 @@
+extends MarginContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func quit(_button):
+	print("HOLA")
+	get_tree().quit()
