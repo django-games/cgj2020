84621fc553092a004815351a6ef1b8a3e1d3725e
Andrés Ignacio Torres <andresitorresm@gmail.com>
2020-02-01 10:34:56 -0400

    add intro scene

---
+++ scenes/IntroTextHolder.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var texts = []
+
+func _ready():
+	for text in self.get_children():
+		print
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
