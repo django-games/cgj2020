187deba2eaab824ed88fd7b366a3a18c67d9b596
Andrés Ignacio Torres <andresitorresm@gmail.com>
2020-02-01 10:40:20 -0400

    add triggers for credits & menu

--- scripts/global.gd
+++ scripts/global.gd
@@ -5,4 +5,11 @@ func _ready():
 
 func _input(_ev):
 	if Input.is_key_pressed(KEY_Q):
-		get_tree().quit()
+		print("Triggering: quit")
+		return get_tree().quit()
+	elif Input.is_key_pressed(KEY_M):
+		print("Triggering: go to Main Menu")
+		return get_tree().change_scene("res://scenes/MenuScene.tscn")
+	elif Input.is_key_pressed(KEY_C):
+		print("Triggering: go to Credits")
+		return get_tree().change_scene("res://scenes/CreditsScene.tscn")
