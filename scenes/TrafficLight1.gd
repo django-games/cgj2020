ada1f0ab38bf0d42ae93a82d73ff90e64ce0c312
Gustavo Castellanos <gustavoaca1997@gmail.com>
2020-02-01 16:20:31 -0400

    Add three colors to traffic lights

--- scenes/TrafficLight1.gd
+++ scenes/TrafficLight1.gd
@@ -1,10 +1,6 @@
-extends Node
+extends Node2D
 
 # Declare member variables here. Examples:
-export var total_green = 0
-
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
