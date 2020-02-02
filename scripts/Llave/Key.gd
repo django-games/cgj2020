45c36b42eda6400b9884a78d2484c52d27b8d223
Andrés Ignacio Torres <andresitorresm@gmail.com>
2020-02-02 08:22:54 -0400

    key to aftermath

--- scripts/Llave/Key.gd
+++ scripts/Llave/Key.gd
@@ -7,3 +7,7 @@ func _on_Area2D_body_entered(body):
 	$Timer2.stop()
 	$Area2D.set_physics_process(false)
 	$Label.show()
+	$TimerChangeScene.start()
+
+func _on_TimerChangeScene_timeout():
+	return get_tree().change_scene("res://scenes/KeyToLetterScene.tscn")
