a1c770408287866aedeee2d0ef150ab7a380ad02
Andrés Ignacio Torres <andresitorresm@gmail.com>
2020-02-02 10:54:40 -0400

    maneja cafe drag & drop

--- scripts/Coffee/Coffee.gd
+++ scripts/Coffee/Coffee.gd
@@ -4,9 +4,12 @@ var val_d = false
 var gus_d = false
 var david_d = false
 var andres_d = false
-func _ready():
-	pass
-	
+
+var amount_ready = 0
+
+func _process(_delta):
+	if amount_ready == 4:
+		print("NEXT SCENE")
 
 func _on_Valerie_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
@@ -40,3 +43,8 @@ func _on_Andres_input_event(_viewport, event, _shape_idx):
 			$Andy/Texto.hide()
 		andres_d = not andres_d
 
+func _on_David_area_entered(area):
+	if area.get_name() == "Cafe":
+		amount_ready += 1
+		$Cafe.hide()
+		print("David con cafe")
