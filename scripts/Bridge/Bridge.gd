b2fdf88b35115252e708baac5294357ff5af613e
David Rodriguez <dave00dark@gmail.com>
2020-02-01 20:56:03 -0400

    Puentecito listooo

---
+++ scripts/Bridge/Bridge.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var is_bridge_complete = 0
+var car
+var SPEED = 150
+
+func _ready():
+	car = $Sprite
+	
+func _process(delta):
+	if is_bridge_complete == 7:
+		print("Siguiente Escena")
+		$Label.show()
+		is_bridge_complete += 1
+		
+	if is_bridge_complete == 8:
+		if car.position.x <= 1200:
+			car.position.x += SPEED * delta
+	
---
+++ scripts/Bridge/BrokenWood.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+var texture = preload("res://assets/Bloque2.png")
+var wood_repared = true
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT and wood_repared:
+		var sprite = get_node("Sprite")
+		sprite.set_texture(texture)
+		get_parent().get_parent().is_bridge_complete += 1
+		wood_repared = false
