9be6b502b92a224ff4b946dacb7752c6e5164ebc
Andrés Ignacio Torres <andresitorresm@gmail.com>
2020-02-02 09:08:24 -0400

    connect letter to car race

---
+++ scenes/LetterToCarRaceScene.gd
@@ -0,0 +1,29 @@
+extends MarginContainer
+
+var texts = []
+var current_node = -1
+var is_last_node = false
+
+func _ready():
+	for text in get_children():
+		texts.append(text)
+	if texts.size() > 0:
+		texts[0].IS_ACTIVE = true
+		current_node = 0
+		for i in range(1, texts.size()):
+			texts[i].IS_ACTIVE = false
+			texts[i].IS_READY = false
+
+func _process(_delta):
+	if current_node >= texts.size():
+		after_last_text()
+	else:
+		var current_text = texts[current_node]
+		if current_text.IS_READY:
+			current_text.IS_ACTIVE = false
+			current_node += 1
+			if current_node < texts.size():
+				texts[current_node].IS_ACTIVE = true
+
+func after_last_text():
+	return get_tree().change_scene("res://scenes/CarRace.tscn")
