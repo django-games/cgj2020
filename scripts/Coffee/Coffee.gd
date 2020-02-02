25640ad0a46a0548595916b9aeb6fab3e438f2c7
David Rodriguez <dave00dark@gmail.com>
2020-02-02 14:41:18 -0400

    Cuerpos listos con texto

--- scripts/Coffee/Coffee.gd
+++ scripts/Coffee/Coffee.gd
@@ -4,16 +4,11 @@ var val_d = false
 var gus_d = false
 var david_d = false
 var andres_d = false
-var can_grab = false
-var grabbed_offset
 func _ready():
 	pass
 	
-func _physics_process(delta):
-	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_grab:
-        grabbed_offset.position = get_global_mouse_position()
 
-func _on_Valerie_input_event(viewport, event, shape_idx):
+func _on_Valerie_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		if val_d:
 			$Val/Texto.show()
@@ -21,7 +16,7 @@ func _on_Valerie_input_event(viewport, event, shape_idx):
 			$Val/Texto.hide()
 		val_d = not val_d
 
-func _on_Gustavo_input_event(viewport, event, shape_idx):
+func _on_Gustavo_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		if gus_d:
 			$Gus/Texto.show()
@@ -29,7 +24,7 @@ func _on_Gustavo_input_event(viewport, event, shape_idx):
 			$Gus/Texto.hide()
 		gus_d = not gus_d
 
-func _on_David_input_event(viewport, event, shape_idx):
+func _on_David_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		if david_d:
 			$Dave/Texto.show()
@@ -37,7 +32,7 @@ func _on_David_input_event(viewport, event, shape_idx):
 			$Dave/Texto.hide()
 		david_d = not david_d
 
-func _on_Andres_input_event(viewport, event, shape_idx):
+func _on_Andres_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		if andres_d:
 			$Andy/Texto.show()
@@ -45,13 +40,3 @@ func _on_Andres_input_event(viewport, event, shape_idx):
 			$Andy/Texto.hide()
 		andres_d = not andres_d
 
-
-func _on_Valerie_body_entered(body):
-	print("ENTRO")
-
-
-func _on_Cafe_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		print("AAAAAAAAAAAAA")
-		can_grab = event.pressed
-		grabbed_offset = $Cafe
